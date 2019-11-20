4a58f1e8f69e0e29fc00b591c433cf3c08c1b81b
Josh Kleymeyer <josh.kleymeyer@gmail.com>
2019-11-19 20:02:38 -0500

    added retrieve by id

--- ch04_favorite_things.js
+++ ch04_favorite_things.js
@@ -8,29 +8,42 @@ let favorites = [];
 //prompt
 let choice = "y";
 while (choice == "y") {
-console.log("=======Add a Favorite Thing========");   
+    console.log("=======Add a Favorite Thing========");
 
-let id_in = readline.questionInt("Favorite id? ");
-let thing_in = readline.question("Favorite thing? ");
-let why_in = readline.question("Why is it a favorite? ");
+    let id_in = readline.questionInt("Favorite id? ");
+    let thing_in = readline.question("Favorite thing? ");
+    let why_in = readline.question("Why is it a favorite? ");
 
-//create favorite
-let favorite = {
-    id: id_in,
-    thing: thing_in,
-    why: why_in
-};
+    //create favorite
+    let favorite = {
+        id: id_in,
+        thing: thing_in,
+        why: why_in
+    };
 
-//add to array
-favorites.push(favorite);
+    //add to array
+    favorites.push(favorite);
 
-//repeat
-choice = readline.question("Add another favorite? (y/n)");
+    //repeat
+    choice = readline.question("Add another favorite? (y/n)");
 }
 
-//display array
 console.log("\nMy list of favorites: ");
-for (let f of favorites){
-console.log(`I like ${f.thing} because ${f.why}.`);
+for (let f of favorites) {
+    displayFavorite(f);
 }
+
+console.log("\nRetrieve a favorited by id");
+let idRetrieved = readline.questionInt("Which favorite (by Id) do you want to retrieve?");
+for (let f of favorites) {
+    if (f.id == idRetrieved) {
+        displayFavorite(f);
+    }
+}
+
+
+function displayFavorite(f) {
+    console.log(`I like ${f.id} - ${f.thing} because ${f.why}.`);
+}
+
 console.log("\nBye");
